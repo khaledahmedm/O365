96d79031c97bceb791125a4f44f5fde45f357857
khaledahmedm <106169745+khaledahmedm@users.noreply.github.com>
2022-07-04 01:40:54 +0200

    Add files via upload

---
+++ Connect to EXO 365 for the first time.ps1
@@ -0,0 +1,11 @@
+﻿Set-ExecutionPolicy RemoteSigned
+
+Install-Module ExchangeOnlineManagement
+
+Import-Module ExchangeOnlineManagement
+
+Connect-ExchangeOnline
+
+#If all went well, you should be able to run Exchange PowerShell for your Office 365. Test the following command to get a list of all your meeting rooms resources:
+
+Get-Mailbox -Filter '(RecipientTypeDetails -eq "RoomMailBox")'
